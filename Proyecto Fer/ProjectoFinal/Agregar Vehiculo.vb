be8b4f4636158fbae1d0d92c145acca8f77a34f7
tonioqiu <tonioqiu.dev@gmail.com>
2021-12-09 05:24:28 -0500

    Add files via upload

---
+++ Proyecto Fer/ProjectoFinal/Agregar Vehiculo.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form2
+    Dim connection As New SqlConnection("Data Source=.;Initial Catalog=db_Delta;Integrated Security=True")
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        connection.Open()
+        Dim cmd As New SqlCommand()
+        cmd.Connection = connection
+        cmd.CommandType = CommandType.StoredProcedure
+        cmd.Parameters.Add(New SqlParameter("@NUMPLACA", TextBox1.Text))
+        cmd.Parameters.Add(New SqlParameter("@MARCA", TextBox2.Text))
+        cmd.Parameters.Add(New SqlParameter("@DISTRIBUIDOR", TextBox3.Text))
+        cmd.Parameters.Add(New SqlParameter("@TIPO_MOTOR", TextBox4.Text))
+        cmd.Parameters.Add(New SqlParameter("@TIPO_VEHICULO", TextBox5.Text))
+        cmd.Parameters.Add(New SqlParameter("@AUTO_ANIO", TextBox6.Text))
+        cmd.Parameters.Add(New SqlParameter("@PRECIO_ALQUILER", TextBox7.Text))
+        cmd.CommandText = "INSERTAR_AUTO"
+        cmd.ExecuteNonQuery()
+        connection.Close()
+        Form1.showdata()
+        Me.Hide()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
