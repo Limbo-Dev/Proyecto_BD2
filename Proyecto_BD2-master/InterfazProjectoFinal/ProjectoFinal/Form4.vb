e21b630c3552d85be929acf18a6ea777844e9ff4
David Torres <63114633+DavidHayter00@users.noreply.github.com>
2021-12-09 04:26:10 -0500

    Add files via upload

---
+++ Proyecto_BD2-master/InterfazProjectoFinal/ProjectoFinal/Form4.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+Public Class Form4
+    Dim connection As New SqlConnection("Data Source=.;Initial Catalog=db_Delta;Integrated Security=True")
+    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        connection.Open()
+        Dim QueryString As String = "select * from DISTRIBUIDOR"
+        Dim table As New DataTable()
+        Dim Command As New SqlCommand(QueryString, connection)
+        Dim adapter As New SqlDataAdapter(Command)
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+        connection.Close()
+    End Sub
+End Class
